fbf9c9124cbc42b1cf871ba65c2999d528385091
Alexander Sopin <alex.sopin0803@gmail.com>
2023-05-12 14:07:45 +0500

    code cleanup

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,47 +1,12 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ImagesService } from './images.service';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
-import JwtAuthenticationGuard from 'src/authentication/jwt-authentication.guard';
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
-  /*   @Post()
-  @UseGuards(JwtAuthenticationGuard)
-  create(@Body() createImageDto: CreateImageDto) {
-    return this.imagesService.create(createImageDto);
-  } */
-
   @Get()
   findAll() {
     return this.imagesService.findAll();
   }
-
-  /*   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.imagesService.findOne(+id);
-  } */
-
-  /*   @Patch(':id')
-  @UseGuards(JwtAuthenticationGuard)
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imagesService.update(+id, updateImageDto);
-  } */
-
-  /*   @Delete(':id')
-  @UseGuards(JwtAuthenticationGuard)
-  remove(@Param('id') id: string) {
-    return this.imagesService.remove(+id);
-  } */
 }
